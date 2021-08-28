4eb47e7313b2c73048f240e4a4d36e9b8a20b32b
Khoa Tran <ttmk84@gmail.com>
2021-08-28 12:47:32 +0700

    them ghi chu

--- javaStream/InStream1.java
+++ javaStream/InStream1.java
@@ -4,15 +4,17 @@ import java.io.*;
 
 public class InStream1 {
 	public static void main(String args[]) {
-		InputStream is = System.in;//keyboard = system.in
-		while(true) {
+		InputStream is = System.in;// keyboard = system.in
+		while (true) {
 			try {
 				int ch = is.read();
-				if(ch == -1 || ch == 'q') break;
-				System.out.println((char)ch);
-			}catch (IOException ie) {
-				System.out.println("Error: "+ie);
+				if (ch == -1 || ch == 'q')
+					break;
+				System.out.println((char) ch);
+			} catch (IOException ie) {
+				System.out.println("Error: " + ie);
 			}
-	}
+		}
 	}
 }
+// ghi chu
